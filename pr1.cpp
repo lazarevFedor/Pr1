c7d4fc20479fe6fd381514be68ce681d67ff4ad9
lazarevFedor <147192648+lazarevFedor@users.noreply.github.com>
2023-12-11 22:56:06 +0300

    Update pr1.cpp

--- pr1.cpp
+++ pr1.cpp
@@ -1,43 +1,21 @@
-#include "iostream"
+#include <iostream>
+
 using namespace std;
-
-void idz13(int& num, int& order, int& mask) {
-    order = sizeof(num) * 8 - 1;
-    mask = 1 << order;
-    cout << "\nПредставление числа по идз: ";
-
-    if (num < 0) {
-        for (int i = 0; i < sizeof(num) * 8; i += 2) {
-            num &= ~(1 << i);
-        }
-    }
-    else {
-        for (int i = 1; i < sizeof(num) * 8; i += 2) {
-            num |= (1 << i);
-        }
-    }
-    for (int i = 0; i <= order; i++) {
-        cout << ((num & mask) ? 1 : 0);
-        num = num << 1;
-
-        if (!i)
-            cout << " ";
-    }
-}
 
 int main() {
     setlocale(LC_ALL, "RU");
     int numOption, number, num, mask, order;
     while (true) {
-
-        cout << "Какое задание хотите проверить?" << endl;
-        cout << "Введите ответ: ";
+        cout << "Выбор задания:\n" << 
+            "1) Занимаемая память\n" <<
+            "2) Двоичное представление целого числа\n" <<
+            "3) Двоичное представление вещ.числа\n" <<
+            "4) Выход\n-->> ";
         cin >> numOption;
         switch (numOption) {
         case 1:
-            cout << "\nРешение 1 задания: \n";
             cout <<
-                "Количсетво занимаемой памяти следующими типами данных:\n" <<
+                "\n\nКоличсетво занимаемой памяти следующими типами данных:\n" <<
                 "int: " << sizeof(int) << "\n" <<
                 "short int: " << sizeof(short int) << "\n" <<
                 "long int: " << sizeof(long int) << "\n" <<
@@ -45,12 +23,11 @@ int main() {
                 "double: " << sizeof(double) << "\n" <<
                 "long double: " << sizeof(long double) << "\n" <<
                 "char: " << sizeof(char) << "\n" <<
-                "bool: " << sizeof(bool) << "\n";
+                "bool: " << sizeof(bool) << "\n\n";
             break;
 
         case 2:
-            cout << "\nРешение 2 задания:\n";
-            cout << "Введите целое число: ";
+            cout << "\n\nВведите целое число: ";
             cin >> number;
             num = number;
             order = sizeof(number) * 8 - 1;
@@ -64,20 +41,18 @@ int main() {
                 if (!i)
                     cout << " ";
             }
-            cout << endl;
-            idz13(num, order, mask);
+            cout << "\n\n";
             break;
 
         case 3:
-            cout << "\nРешение 3 задания: " << endl;
-            int order = sizeof(int) * 8 - 1;
-            int mask = 1 << order;
+            order = sizeof(int) * 8 - 1;
+            mask = 1 << order;
             union
             {
                 int intI;
                 float floatF;
             };
-            cout << "Введите вещественное число: ";
+            cout << "\n\nВведите вещественное число: ";
             cin >> floatF;
             num = intI;
             cout << "\nДвоичное представление: ";
@@ -89,16 +64,14 @@ int main() {
                     cout << " ";
 
             }
-            idz13(num, order, mask);
+            cout << "\n\n";
             break;
-        }
-        cout << "\n\nВыбрать другое задание?(1 - да, 0 - нет)\n";
-        cout << "Введите ответ: ";
-        cin >> numOption;
-        if (numOption != 1) {
+        case 4:
+            exit(0);
             break;
+        default:
+            cout << "\nОшибка! Попробуйте снова\n";
         }
-        cout << endl;
     }
     return 0;
 }
